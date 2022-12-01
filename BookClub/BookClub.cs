ea6f511e8ccad3a1fad9adbbab7e352169b9566a
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-12-01 11:19:52 -0500

    adding the xmle linqs in the bookclub class

--- BookClub/BookClub.cs
+++ BookClub/BookClub.cs
@@ -1,4 +1,5 @@
-
+using System.Xml.Linq;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace BookClub
@@ -6,9 +7,11 @@ namespace BookClub
     public class BookClub : IBookClub
     {
         private List<IBook> _books = new List<IBook>();
+        
 
         public BookClub(string filePath) {
-
+            XElement booksXml = XElement.Load(filePath + "books.xml");
+            XElement ratingsXml = XElement.Load(filePath + "ratings.xml");
         }
 
         public List<IBook> Books {
