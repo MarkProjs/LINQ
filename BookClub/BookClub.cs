01320cdfa7076141ca1d8613f69d218a93ebe0c8
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-12-01 10:50:17 -0500

    adding a constructor and the necessary methods for the bookclub which implements the IBookclub

--- BookClub/BookClub.cs
+++ BookClub/BookClub.cs
@@ -1,9 +1,24 @@
+
+using System.Collections.Generic;
+
 namespace BookClub
 {
     public class BookClub : IBookClub
     {
+        private List<IBook> _books = new List<IBook>();
+
         public BookClub(string filePath) {
-            
+
+        }
+
+        public List<IBook> Books {
+            get {
+                return _books;
+            }
+        }
+
+        public void LoadData() {
+
         }
     }
 }
