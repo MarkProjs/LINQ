8226940f42e5fbd7d23916f914bc3bbd97677dcb
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-12-01 10:35:52 -0500

    doing the book class and overriding its ToString

---
+++ BookClub/Book.cs
@@ -0,0 +1,36 @@
+namespace BookClub
+{
+    public class Book : IBook
+    {
+        public Book(int _bookId, string _title, string _description, string _genre, string _authorLastName, string _authorFirstName, double _rating, int _numberOfReaders) {
+            BookId = _bookId;
+            Title = _title;
+            Description = _description;
+            Genre = _genre;
+            AuthorLastName = _authorLastName;
+            AuthorFirstName = _authorFirstName;
+            Rating = _rating;
+            NumberOfReaders = _numberOfReaders;
+        }
+
+        public int BookId {get;}
+        public string Title {get;}
+        public string Description {get;}
+        public string Genre {get;}
+        public string AuthorLastName {get;}
+        public string AuthorFirstName {get;}
+        public double Rating {get;}
+        public int NumberOfReaders {get;}
+
+        public override string ToString() {
+            return BookId + "\n" +
+                    Title + "\n" +
+                    "Desc: " + Description + "\n" +
+                    "Genre: " +Genre + "\n" +
+                    AuthorLastName + ", "+ AuthorFirstName + "\n" +
+                    "Rating: " + Rating + "\n" +
+                    "Number Of Readers: " + NumberOfReaders;
+        }
+        
+    }
+}
