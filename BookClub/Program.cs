e6d6e169822e4d8aa40bdc0ba35eac809dc09f3c
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-12-08 15:08:18 -0500

    adding a new option

--- BookClub/Program.cs
+++ BookClub/Program.cs
@@ -18,12 +18,6 @@ namespace BookClub
                     BookClub bc = new BookClub(filePath.ToLower());
                     bc.LoadData();
                     List<IBook> books = bc.Books;
-                    //print the books and the ratings
-                    foreach(Book aBook in books)
-                    {
-                        Console.WriteLine(aBook);
-                    }
-
                     isTrue = DisplayMenu(books, isTrue);
 
                 } catch(Exception e) {
@@ -43,11 +37,12 @@ namespace BookClub
                 {   
                     Console.WriteLine("-----------------------------------------------------------------------MAIN MENU---------------------------------------------------------------------------------------------------------------");
                     Console.WriteLine();
-                    Console.WriteLine("1. View the top-rated books. Press 1");
-                    Console.WriteLine("2. Browse books by popular genre. Press 2");
-                    Console.WriteLine("3. Search a book by keyword. Press 3");
-                    Console.WriteLine("4. Most popular book per its genre. Press 4");
-                    Console.WriteLine("5. Exit the program. Press 5");
+                    Console.WriteLine("1. View all the Books");
+                    Console.WriteLine("2. View the top-rated books. Press 2");
+                    Console.WriteLine("3. Browse books by popular genre. Press 3");
+                    Console.WriteLine("4. Search a book by keyword. Press 4");
+                    Console.WriteLine("5. Most popular book per its genre. Press 5");
+                    Console.WriteLine("6. Exit the program. Press 6");
                     Console.WriteLine();
                     Console.Write("What would you like to see? ");
                     string userInput = Console.ReadLine();
@@ -55,6 +50,14 @@ namespace BookClub
                     switch(userInput)
                     {
                         case "1":
+                            //print the books and the ratings
+                            foreach(Book aBook in books)
+                            {
+                                Console.WriteLine(aBook);
+                            }
+                            break;
+                            
+                        case "2":
                             var top5Books = (from b in books
                                              where b.NumberOfReaders > 20 && b.Rating > 3
                                              orderby b.Rating descending
@@ -65,8 +68,8 @@ namespace BookClub
                                 Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
                             }
                             Console.WriteLine("These are the top 5 books");      
-                            break;
-                        case "2":
+                            break;  
+                        case "3":
                             // grouping the book by its genre
                             var GenreQuery1 = from b in books
                                              group b by b.Genre;
@@ -91,7 +94,8 @@ namespace BookClub
 
                             Console.WriteLine();
                             break;
-                        case "3":
+                            
+                        case "4":
                             Console.Write("Keyword to search: ");
                             string UserKeyWord = Console.ReadLine();
 
@@ -105,18 +109,19 @@ namespace BookClub
                                 Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
                             }         
                             break;
-                        case "4":
+                            
+                        case "5":
                             // grouping the book by its genre
                             var GenreQuery = from b in books
-                                                    group b by b.Genre;
-                            
+                                            group b by b.Genre;
+                                
                             //looping through the grouping
                             foreach(var GenreGroup in GenreQuery) {
                                 //selecting the top book by its genre
                                 var TopBookPerGenre = (from g in GenreGroup
-                                                       orderby g.Rating descending
+                                                        orderby g.Rating descending
                                                         select g).Take(1);
-                                
+                                    
                                 //print the top books by its genre
                                 foreach(var TopBook in TopBookPerGenre) {
                                     Console.WriteLine(TopBook);
@@ -125,7 +130,7 @@ namespace BookClub
                             }
 
                             break;
-                        case "5":
+                        case "6":
                             Console.WriteLine("Have a Good day!");
                             menu = false;
                             break;
